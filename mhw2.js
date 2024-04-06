f6c5fc36a1c3793077e9900345b5a100ed28288b
Carmelo-02 <carmeloscaccianoce@icloud.com>
2024-04-06 16:58:41 +0200

    Submission

--- mhw2.js
+++ mhw2.js
@@ -1,10 +1,11 @@
 const header_elms = document.querySelectorAll('.fade-opacity');
 const header_bg = document.querySelector('.header-bg');
 const header = document.querySelector('header');
+const sections = document.querySelectorAll('#tiles section');
 
 let lastScrollTop = 0;
 
-function fade() {
+function fadeHeader() {
     const currentScroll = window.scrollY || document.documentElement.scrollTop;
 
     if (currentScroll > lastScrollTop) {
@@ -23,16 +24,16 @@ function fade() {
 }
 
 function toggleHeaderTransictions() {
-    if (window.innerWidth > 1256) {
+    if (window.innerWidth > 960) {
         header_bg.classList.remove('hidden');
 
-        window.addEventListener('scroll', fade);
+        window.addEventListener('scroll', fadeHeader);
     }
     else {
         header_elms.forEach((item) => {item.style.opacity = '1';});
         header_bg.classList.add('hidden');
 
-        window.removeEventListener('scroll', fade);
+        window.removeEventListener('scroll', fadeHeader);
     }
 }
 
@@ -40,9 +41,31 @@ function shadowing(nodeList) {
     nodeList.forEach((item) => {item.classList.add('shadowed');});
 }
 
+function selectImageSource(nodeList) {
+    nodeList.forEach((item) => {
+        const image = item.querySelector('img');
+
+        if (window.innerWidth > 960)
+            image.src = item.dataset.desktop;
+        else
+            image.src = item.dataset.mobile;
+    });
+}
+
+function addImages(nodeList) {
+    nodeList.forEach((item) => {
+        const image = document.createElement('img');
+        item.prepend(image);
+    })
+}
+
+addImages(sections);
+selectImageSource(sections);
+
 const h2_articles = document.querySelectorAll('#inner-section article h2');
 shadowing(h2_articles);
 
 toggleHeaderTransictions();
 
 window.addEventListener('resize', toggleHeaderTransictions);
+window.addEventListener('resize', function() {selectImageSource(sections)});
